e2d17163381014dd214757414996bf7f6ae13e7c
kik <kik@kik>
2017-02-10 17:22:49 +0800

    系統預設印表機proxy

---
+++ PrinterSetting/Util/OSDefaultPrinterProxy.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.Runtime.InteropServices;
+
+
+namespace PrinterSetting.Util
+{
+    public class OSDefaultPrinterProxy
+    {
+
+        private const long HWND_BROADCAST = 0xffffL;
+        private const long WM_WININICHANGE = 0x1a;
+
+        [DllImport("user32.dll", EntryPoint = "SendMessageA")]
+        private static extern int SendMessage(long hwnd, long wMsg, long wParam, string lParam);
+        
+        [DllImport("kernel32.dll")]
+        static extern bool WriteProfileString(string lpAppName, string lpKeyName, string lpString);
+
+
+        /// <summary>
+        /// 設定作業系統預設印表機。True:代表更新成功，False:代表更新失敗。
+        /// </summary>
+        /// <param name="SelectedPrinterName">印表機名稱</param>
+        public bool SetDefaultPrinter(string SelectedPrinterName)
+        {
+            try
+            {
+                // 使用 WriteProfileString 設定預設印表機
+                WriteProfileString("windows", "Device", (SelectedPrinterName + ",,"));
+
+                // 使用 SendMessage 傳送正確的通知給所有最上層的層級視窗。
+                // WIN.INI 要在意的應用程式接聽此訊息，並且視需要重新讀取 WIN.ini
+                SendMessage(HWND_BROADCAST, WM_WININICHANGE, 0, "windows");
+
+                return true;
+            }
+            catch
+            {
+                return false;       
+            }
+        }
+
+        /// <summary>
+        /// 取得作業系統目前的預設印表機名稱。True:代表有找到，False:代表沒有找到，回傳值沒有意義。
+        /// </summary>
+        /// <param name="OSDefaultPrinter">回傳的作業系統目前的預設印表機名稱</param>
+        public bool GetOSDefaultPrinter(out string OSDefaultPrinter)
+        {
+            try
+            {
+                PrintDocument pd = new PrintDocument();
+
+                OSDefaultPrinter = pd.PrinterSettings.PrinterName;
+
+                return true;
+
+            }
+            catch
+            {
+                OSDefaultPrinter = string.Empty;
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 取得作業系統所有已安裝的印表機名稱。True:代表有找到，False:代表沒有找到，回傳值沒有意義。
+        /// </summary>
+        /// <param name="OSPrinterList">回傳的印表機清單</param>
+        public bool GetOSPrinterList(out List<string> OSPrinterList)
+        {
+            try
+            {
+                PrintDocument printDoc = new PrintDocument();
+                string OSdefaultPrinter;
+
+
+                OSPrinterList = new List<string>();
+                OSdefaultPrinter = printDoc.PrinterSettings.PrinterName;
+
+
+                foreach (string printerName in PrinterSettings.InstalledPrinters)
+                {
+                    OSPrinterList.Add(printerName);
+                }
+
+                return true;
+
+            }
+            catch(Exception EX)
+            {
+                OSPrinterList = null;
+                return false;
+            }
+        }
+
+
+    }
+}
---
+++ testOSDefaultPrinterProxy/testOSDefaultPrinterProxy.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.Linq;
+using PrinterSetting.Util;
+
+
+namespace testOSDefaultPrinterProxy
+{
+    [TestClass]
+    public class testOSDefaultPrinterProxy
+    {
+        private OSDefaultPrinterProxy osPrintProxy = new OSDefaultPrinterProxy();
+
+
+        [TestMethod]
+        public void TestGetOSDefaultPrinter()
+        {
+            string printerName;
+            string expect = new PrintDocument().PrinterSettings.PrinterName;
+
+
+            if (!osPrintProxy.GetOSDefaultPrinter(out printerName))
+            {
+                throw new NullReferenceException("取得系統預設印表機失敗");
+            }
+
+
+            if (!expect.Equals(printerName))
+            {
+                throw new ArgumentException("Method failed");
+            }
+
+        }
+
+        [TestMethod]
+        public void TestGetOSPrinterList()
+        {
+            List<string> printerList;
+            string errorPrinterName = "errorPrinter";
+            
+
+            if (!osPrintProxy.GetOSPrinterList(out printerList))
+            {
+                throw new NullReferenceException("查無印表機清單");
+            }
+
+
+            foreach (string printerName in PrinterSettings.InstalledPrinters)
+            {
+
+                if (!printerList.Contains(printerName))
+                {
+                    throw new ArgumentException("Method failed");
+                }
+            }
+
+
+            if (printerList.Contains(errorPrinterName))
+            {
+                throw new ArgumentException("Method failed");
+            }
+             
+        }
+
+        [TestMethod]
+        public void TestSetDefaultPrinter()
+        {
+            string expectPrinterName = "Fax";
+            string errorPrinterName = "沒有設定預設印表機。";
+            string testPrinterName;
+
+            if (!osPrintProxy.SetDefaultPrinter(expectPrinterName))
+            {
+                throw new NullReferenceException("Method failed");
+            }
+
+            testPrinterName = new PrintDocument().PrinterSettings.PrinterName;
+
+            if (!expectPrinterName.Equals(testPrinterName))
+            {
+                throw new ArgumentException("系統預設印表機設定值不符合測試期望值");
+            }
+
+
+            if (!osPrintProxy.SetDefaultPrinter(string.Empty))
+            {
+                throw new NullReferenceException("Method failed");
+            }
+
+            testPrinterName = new PrintDocument().PrinterSettings.PrinterName;
+
+            if (!errorPrinterName.Equals(testPrinterName))
+            {
+                throw new ArgumentException("沒有取消系統預設印表機");
+            }
+
+        }
+
+
+    }
+}
